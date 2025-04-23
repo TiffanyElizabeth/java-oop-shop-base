91c3882f9e9e4761988e0ed661e865db7c1d6e03
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-04-23 02:57:15 +0200

    add prodotto

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,41 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+    // characteristics
+    public int code;
+    public String name;
+    public String codeName; // = code + "-" + name;
+    public String description;
+    public double basePrice;
+    public double vat;
+    public double finalPrice; // = (basePrice * vat) + basePrice;
+    public boolean isAvailable;
+
+    // methods
+    public void checkStock() {
+        this.isAvailable = true;
+    }
+
+    public String getCodeName() {
+        this.codeName = this.code + "-" + this.name;
+        return this.codeName;
+    }
+
+    public double getFinalPrice() {
+        this.finalPrice = (this.basePrice * this.vat) + this.basePrice;
+        return finalPrice;
+    }
+
+    // constructor
+    public Prodotto(String name, String description, double basePrice, double vat) {
+        Random random = new Random();
+        this.code = random.nextInt(999999);
+        this.name = name;
+        this.description = description;
+        this.basePrice = basePrice;
+        this.vat = vat;
+        this.isAvailable = false;
+    }
 }
