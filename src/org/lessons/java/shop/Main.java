3a222496401202ed7eceb38124386aaca70de5c3
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-04-23 02:57:28 +0200

    add sample prodotto

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,5 +1,12 @@
 package org.lessons.java.shop;
 
 public class Main {
+    public static void main(String[] args) {
+        Prodotto blueJacket = new Prodotto("blue jacket", "waterproof women's blue rain jacket", 250, 0.24);
 
+        System.out.println(blueJacket.basePrice);
+        System.out.println(blueJacket.getFinalPrice());
+        System.out.println(blueJacket.code);
+        System.out.println(blueJacket.getCodeName());
+    }
 }
