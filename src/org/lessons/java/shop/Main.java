55394aab2f90f8681da1719ace5e2b7fe478a3ed
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-04-25 02:28:34 +0200

    added test for second constructor

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,11 +2,21 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto blueJacket = new Prodotto("blue jacket", "waterproof women's blue rain jacket", 229.2, 0.24);
+        Prodotto blueJacket = new Prodotto("blue jacket", "patagonia", 229.2, 0.24);
 
-        System.out.println(blueJacket.basePrice);
+        System.out.println(blueJacket.getBasePrice());
         System.out.println(blueJacket.getFinalPrice());
-        System.out.println(blueJacket.code);
+        System.out.println(blueJacket.getCode());
         System.out.println(blueJacket.getCodeName());
+
+        System.out.println(blueJacket.isAvailable());
+        blueJacket.markAsAvailable();
+        System.out.println(blueJacket.isAvailable());
+
+        Prodotto blackJeans = new Prodotto(900042, "black jeans", "j brand", 200, 0.22);
+
+        System.out.println(blackJeans.getBrand());
+        System.out.println(blackJeans.getCode());
+        System.out.println(blackJeans.getCodeName());
     }
 }
